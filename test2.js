f108742b49ab03c384a756939024ebc1fcf8a587
Sam123 <32543977+sampathracherla@users.noreply.github.com>
2018-01-29 23:36:29 -0500

    Add files via upload

---
+++ test2.js
@@ -0,0 +1,11 @@
+describe('google images homepage', function() {
+      it('go to google images page', function() {
+		  browser.driver.get('https://www.dice.com/');
+		  browser.driver.manage().window().maximize();
+          browser.driver.findElement(by.name('q')).sendKeys('QA Manager');
+		  browser.driver.findElement(by.className('btn btn-primary btn-lg btn-block')).click();
+		  browser.driver.findElement(by.id('sort-by-date-link')).click();
+		  browser.driver.sleep(100000);
+		});
+});
+
