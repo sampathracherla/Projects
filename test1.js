ca75320ddc75e0378c8826086934bf5c23da2bf3
Sam123 <32543977+sampathracherla@users.noreply.github.com>
2018-01-29 23:40:04 -0500

    Delete test1.js

--- test1.js
+++
@@ -1,9 +0,0 @@
-describe('google images homepage', function() {
-      it('go to google images page', function() {
-		  browser.driver.get('https://images.google.com/?gws_rd=ssl');
-          browser.driver.findElement(by.name('q')).sendKeys('hills');
-		  browser.driver.findElement(by.name('btnG')).click();
-		  browser.driver.findElement(by.name('yUUQhHPLNLSfvM:')).click();
-		  browser.driver.sleep(10000);
-		});
-});
