de00d2681df73b92a56637c50f40010828a39679
Sam123 <32543977+sampathracherla@users.noreply.github.com>
2018-08-24 20:51:07 -0400

    Update test2.js

--- ProtractorExamples/test2.js
+++ ProtractorExamples/test2.js
@@ -1,6 +1,6 @@
 describe('google images homepage', function() {
       it('go to google images page', function() {
-		  browser.driver.get('https://www.dice.com/');
+		  browser.driver.get('https://www.google.com/');
 		  browser.driver.manage().window().maximize();
           browser.driver.findElement(by.name('q')).sendKeys('QA Manager');
 		  browser.driver.findElement(by.className('btn btn-primary btn-lg btn-block')).click();
